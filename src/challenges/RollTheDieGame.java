3f95ea5a63b1573d2a054b97dacfd6cd19018173
ceoDemitri <demichole@gmail.com>
2023-11-05 10:35:31 -0600

    Challenge: I Did not complete this on my own, I followed the guide to not waste time. I will revisit to ensure I know what is going on

---
+++ src/challenges/RollTheDieGame.java
@@ -0,0 +1,37 @@
+package challenges;
+
+import java.util.Random;
+
+public class RollTheDieGame {
+    public static void main(String[] args){
+        int lastSpace = 20;
+        int currentSpace = 0;
+        int maxRolls = 5;
+        Random random = new Random();
+
+        System.out.println("Welcome to Roll the Die! Let's Begin ...");
+
+        for(int i=1; i<maxRolls; i++){
+
+            int die = random.nextInt(bound:6) + 1;
+            currentSpace = currentSpace + die;
+
+            System.out.print(String.format("Roll #%d. You've rolled a %d. ", i, die));
+
+            if(currentSpace == lastSpace){
+                System.out.print("You are on space " + currentSpace + ". Congrats, you win!");
+                break;
+            } else if (currentSpace > lastSpace) {
+                System.out.print("ooh, that takes you past " + lastSpace + " spaces. You lose!");
+            } else if (i == maxRolls && currentSpace < lastSpace) {
+                System.out.println("You are on space " + currentSpace + ".");
+                System.out.println("Unfortunately, you didn't make it to all " + lastSpace + " spaces. You lose!");
+            } else {
+                int spacesToGo = lastSpace - currentSpace;
+                System.out.print("You are now on space " + currentSpace + " and have " + spacesToGo + "  more to go.");
+            }
+
+            System.out.println();
+        }
+    }
+}
