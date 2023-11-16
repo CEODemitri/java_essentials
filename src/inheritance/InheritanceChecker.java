b012f4c682daf1f8d905650297b0d3ce0ed55bfb
ceoDemitri <demichole@gmail.com>
2023-11-15 22:01:57 -0600

    Inheritance : first taste

---
+++ src/inheritance/InheritanceChecker.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class InheritanceChecker {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        //person.
+        Employee employee = new Employee();
+        //employee.
+    }
+}
