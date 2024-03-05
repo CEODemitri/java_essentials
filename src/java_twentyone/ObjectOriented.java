f236be4c1f4fa992444e8e27f154708993746b3b
ceoDemitri <demichole@gmail.com>
2024-03-05 06:15:23 -0600

    Reading the Java 21 Docs. Follwoing Along With Comments for now while reviewing he basics

--- src/java_twentyone/ObjectOriented.java
+++ src/java_twentyone/ObjectOriented.java
@@ -3,34 +3,40 @@ package java_twentyone;
 public class ObjectOriented {
 
     // You may have noticed that the Bicycle class does not contain a main method. That's because it's not a complete application; it's just the blueprint for bicycles that might be used in an application.
-    class Bicycle {
+//    class Bicycle {
+//
+////      state
+//        int cadence = 0;
+//        int speed = 0;
+//        int gear = 1;
+//
+////      behavior: data encapsulation
+//        void changeCadence(int newValue) {
+//            cadence = newValue;
+//        }
+//
+//        void changeGear(int newValue) {
+//            gear = newValue;
+//        }
+//
+//        void speedUp(int increment) {
+//            speed = speed + increment;
+//        }
+//
+//        void applyBrakes(int decrement) {
+//            speed = speed + increment;
+//        }
+//
+//        void printStates() {
+//            System.out.println("cadence: " + cadence + " speed: " + speed + " gear: " + gear);
+//        }
+//    }
 
-//      state
-        int cadence = 0;
-        int speed = 0;
-        int gear = 1;
-
-//      behavior: data encapsulation
-        void changeCadence(int newValue) {
-            cadence = newValue;
-        }
-
-        void changeGear(int newValue) {
-            gear = newValue;
-        }
-
-        void speedUp(int increment) {
-            speed = speed + increment;
-        }
-
-        void applyBrakes(int decrement) {
-            speed = speed + increment;
-        }
-
-        void printStates() {
-            System.out.println("cadence: " + cadence + " speed: " + speed + " gear: " + gear);
-        }
-    }
 }
+// Primitive DATA TYPES: byte, short, int, long, float, double, boolean, char
 
-
+// Declaring an array of integers;
+//    int [] anArray;
+// initialize the values
+//    anArray[0] = 4;
+//    anArray[1] = 300;
