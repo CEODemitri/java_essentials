d79cb1f45da90b17e30423b8b2110c44d2d4b13d
ceoDemitri <demichole@gmail.com>
2024-09-17 22:06:42 -0500

    refresher on inheritance using bicycle class from earlier

--- src/java_twentyone/ObjectOriented.java
+++ src/java_twentyone/ObjectOriented.java
@@ -1,8 +1,10 @@
 package java_twentyone;
 
 public class ObjectOriented {
+//    WHAT IS AN OBJECT
 
-    // You may have noticed that the Bicycle class does not contain a main method. That's because it's not a complete application; it's just the blueprint for bicycles that might be used in an application.
+//    WHAT IS A CLASS
+//    You may have noticed that the Bicycle class does not contain a main method. That's because it's not a complete application; it's just the blueprint for bicycles that might be used in an application.
 //    class Bicycle {
 //
 ////      state
@@ -31,12 +33,12 @@ public class ObjectOriented {
 //            System.out.println("cadence: " + cadence + " speed: " + speed + " gear: " + gear);
 //        }
 //    }
+//
+//    WHAT IS INHERITANCE
+//    create a new file to make the subclass easy to read
+//    class MountainBike extends Bicycle {
+
+    // new fields for methods
+    // mountain bike
 
 }
-// Primitive DATA TYPES: byte, short, int, long, float, double, boolean, char
-
-// Declaring an array of integers;
-//    int [] anArray;
-// initialize the values
-//    anArray[0] = 4;
-//    anArray[1] = 300;
